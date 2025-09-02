cbf76127624be9903aa33a1b856399d1e70e7f8c
阿rong <1940221438@qq.com>
2025-09-02 11:39:48 +0800

    chore: 删除 Lzf 项目中的 .gitignore 文件和 CloudProgram 中的 package-lock.json
    -移除了多个目录下的 .gitignore 文件，包括 Lzf、Application 和 entry 目录
    - 删除了 CloudProgram/node_modules 目录下的 package-lock.json 文件
    - 一并删除了 _tsserver.js 文件，可能与 typescript 相关

--- Lzf/CloudProgram/cloudfunctions/id-generator/idGenerator.ts
+++
@@ -1,9 +0,0 @@
-import * as crypto from 'crypto';
-
-export class IdGenerator {
-  randomUUID() {
-    const uuid = crypto.randomUUID();
-    console.info(`Generate random UUID: ${uuid}`);
-    return { "uuid": uuid };
-  }
-}
